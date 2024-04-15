237419741a70b02633eed83bdb4d0afbaa142292
Malva <malvaorbitadigital@gmail.com>
2024-04-15 12:33:56 +0200

    número de anotaciones en index

--- index.php
+++ index.php
@@ -14,7 +14,18 @@ include_once 'inc_pie.php';
 <body>
     <div class="text-center mt-5">
         <h3>Bienvenido a mi contabilidad doméstica.</h3>
-        <p>Actualmente hay XX anotaciones.</p>
+        <p>Actualmente hay 
+            <?php 
+        
+            $connection = conectar();
+            $stmt = $connection->prepare("SELECT count(*) FROM gastos");
+            $stmt->execute();
+            $stmt->bindColumn(1, $total);
+            $stmt->fetch();
+            echo $total;
+
+            ?>
+         anotaciones.</p>
     </div>
 </body>
 </html>
