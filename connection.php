de44d7fca056f0004f95ad40f53735fb1deb490a
Malva <malvaorbitadigital@gmail.com>
2024-04-15 10:13:54 +0200

    conexión base de datos arreglada

--- connection.php
+++ connection.php
@@ -1,13 +1,12 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "root";
+// $password = "root";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=contab", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=contab", $username);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
