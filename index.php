03003ee5ab1c160cc3b0b2ac145827112c0621a7
Malva <malvaorbitadigital@gmail.com>
2024-04-15 08:59:18 +0200

    html menú y páginas principales

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="d-flex flex-column align-items-center">
+            <button class="btn w-50 p-3 fs-2">Buscar</button>
+            <button class="btn w-50 p-3 fs-2">Listado</button>
+            <button class="btn w-50 p-3 fs-2">Nuevo</button>
+        </div>
+    </div>
+</body>
+</html>
