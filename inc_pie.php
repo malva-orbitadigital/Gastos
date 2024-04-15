1481e3df312596a937d1e672862aef44b1e7eb0e
Malva <malvaorbitadigital@gmail.com>
2024-04-15 11:06:14 +0200

    layout cabecera y pie y html página principal

--- inc_pie.php
+++ inc_pie.php
@@ -1,7 +1,3 @@
-<?php
-include_once 'inc_cabecera.php';
-include_once 'inc_pie.php';
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,11 +6,10 @@ include_once 'inc_pie.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="menu.js"></script>
+    <title></title>
 </head>
 <body>
-    <div class="text-center mt-5">
-        <h3>Bienvenido a mi contabilidad doméstica.</h3>
-        <p>Actualmente hay XX anotaciones.</p>
-    </div>
+    <footer class="db-footer bg-light fixed-bottom p-4">Gestor de gastos</footer>
 </body>
 </html>
