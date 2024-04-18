21f788aaea6b09727f23f56576a9d08405ede615
Malva <malvaorbitadigital@gmail.com>
2024-04-18 10:47:16 +0200

    botones en buscar

--- search.php
+++ search.php
@@ -14,7 +14,7 @@ include_once 'inc_pie.php';
     <?php
     if (isset($_POST['busqueda'])){
         $input = $_POST['busqueda'];
-        echo Expenses::showExpenses("fecha, importe, id", "gastos", "descripcion LIKE '%$input%'", "fecha", "desc", false);
+        echo Expenses::showExpenses("fecha, importe, id", "gastos", "descripcion LIKE '%$input%'", "fecha", "desc", true);
     }
     ?>
 </div>
